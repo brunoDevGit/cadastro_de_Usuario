2b8e199729a1ce5d6e61855462a341b879171ddb
bruno <brunodoliveirapereira@gmail.com>
2020-05-01 00:27:44 -0300

    content

--- frontend/src/components/template/Nav.jsx
+++ frontend/src/components/template/Nav.jsx
@@ -7,10 +7,10 @@ export default props =>
 <aside className= "menu-area">
     <nav className="menu">
     <a href="#/">
-            <i className="fa fa-home">Início</i>
+            <i className="fa fa-home"><b/> Início</i>
         </a>
         <a href="#/users">
-            <i className="fa fa-users">Usuários</i>
+            <i className="fa fa-users"><b/> Usuários</i>
         </a>
     </nav>
 </aside>
--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -4,20 +4,21 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import "font-awesome/css/font-awesome.min.css"
 import './App.css'
 
-import Logo from  '../components/template/Logo'
+import Logo from '../components/template/Logo'
 import Footer from '../components/template/Footer'
 import Nav from '../components/template/Nav'
-import Main from '../components/template/Main'
+
+import Home from '../components/home/Home'
 
 export default props =>
 
-<div className="app">
-        <Logo icon="users"></Logo>
-        
-        <Nav></Nav>
-        <Main  title="Início"
-              subtitle="crud parte frontend">
+      <div className="app">
+            <Logo icon="users"></Logo>
 
-        </Main>
-        <Footer></Footer>
-</div>
+            <Nav>
+                  
+            </Nav>
+            <Home></Home>
+
+            <Footer></Footer>
+      </div>
