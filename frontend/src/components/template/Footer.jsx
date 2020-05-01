c8f93e0fff6850365ca7833a7fb66bc18d046ac8
bruno <brunodoliveirapereira@gmail.com>
2020-04-30 23:41:04 -0300

    navigation

--- frontend/src/components/template/Footer.jsx
+++ frontend/src/components/template/Footer.jsx
@@ -6,6 +6,6 @@ export default props =>
 
 <footer className="footer">
     <span>
-    Desenvolvido por <i className="fa fa-tree text-primary"> <a href="http://brunodevgit.github.io/">brnApps</a> </i>
+    Desenvolvido por <i className="fa fa-tree text-primary"> <a  href="http://brunodevgit.github.io/">brnApps</a> </i>
     </span>
 </footer>
